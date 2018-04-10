0d0c1e6a104d1ff19b8f6213686872b94dadfde0
yurir <yuri.ronin@gmail.com>
2018-04-10 15:28:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,14 +16,11 @@
 
 
 # Install prerequisites.
-#echo going to run 'apt-get update' 
-#apt-get update
+apt-get update
 
-#echo going to run 'apt-get install' 
-#apt-get install -y -q --no-install-recommends \
-#  curl ca-certificates make g++ sudo bash
+apt-get install -y -q --no-install-recommends \
+  curl ca-certificates make g++ sudo bash
 
-echo going to run 'Install Fluentd' 
 # Install Fluentd.
 /usr/bin/curl -sSL https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent2.sh | sh
 
